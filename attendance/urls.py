627d17b9ff45f178990d6aed272929ae7caeb4ed
Satyam Solanki <ridtech.satyam@gmail.com>
2026-06-30 02:27:11 +0530

    added student attendance module

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import student_attendance_view
 
 urlpatterns = [
-    path('', student_attendance_view, name='student_attendance'),
+    path('', student_attendance_view, name='attendance'),          
 ]
